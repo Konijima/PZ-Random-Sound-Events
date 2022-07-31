81af4f8ed0bc7a6aff55d2ead137c9921bbd22b9
Konijima <mathieugermain1991@gmail.com>
2022-07-31 08:35:49 -0400

    Remove unused utilities

--- Contents/mods/RandomSoundEvents/media/lua/shared/RandomSoundEvents/Utils.lua
+++ Contents/mods/RandomSoundEvents/media/lua/shared/RandomSoundEvents/Utils.lua
@@ -1,5 +1,3 @@
-local json = require("MyModName/Libraries/Json");
-
 ---@class Utils
 local Utils = {};
 
@@ -36,67 +34,6 @@ function Utils.GetPlayerFromUsername(username)
         end
     end
     return getPlayerFromUsername(username);
-end
-
----Write a string to a file
----@param filename string
----@param str string
-function Utils.WriteFile(filename, str)
-    local writer = getFileWriter(fileName, true, false);
-    if writer then
-        writer:write(str);
-        writer:close();
-    end
-end
-
----Read a string from a file
----@param filename string
----@return string
-function Utils.ReadFile(filename)
-    local str = "";
-    local reader = getFileReader(filename, false);
-    if reader then
-        while true do
-            local line = reader:readLine()
-            if line == nil then
-                reader:close()
-                break
-            end
-            str = str .. line;
-        end
-    end
-    return str;
-end
-
----Write a table to a file
----@param filename string
----@param tbl table
-function Utils.WriteTable(filename, tbl)
-    local str = Utils.TableToJson(tbl);
-    Utils.WriteFile(filename, str);
-end
-
----Read a table from a file
----@param filename string
----@return table
-function Utils.ReadTable(filename)
-    local str = Utils.ReadFile(filename);
-    local tbl = Utils.JsonToTable(str);
-    return tbl;
-end
-
----Encode a table to a json string
----@param data table
----@return string
-function Utils.TableToJson(data)
-    return json.Encode(data);
-end
-
----Decode a json string to a table
----@param str string
----@return table
-function Utils.JsonToTable(str)
-    return json.Decode(str);
 end
 
 ---Check if xy2 is in range of xy1
